7bd7d7dd287f64b74e454fb017fee14beeecc114
NooMB1T <antonyarmoshuk@gmail.com>
2026-03-24 18:37:46 +0200

    server.js

--- server.js
+++ server.js
@@ -2,35 +2,41 @@ const e = require('express'), h = require('http'), { Server: S } = require('sock
 const a = e(), s = h.createServer(a), o = new S(s, { cors: { origin: "*" } });
 a.use(e.static(__dirname));
 
-let _msgs = {}, _u = {}, _db = {}; 
+let _h = [], _u = {}, _db = {}; 
 
 o.on('connection', (k) => {
     k.on('j', (d) => {
-        k.u = d.u; k.ph = d.ph;
-        _u[d.u] = { id: k.id, av: d.av, ph: d.ph };
-        _db[d.ph] = { u: d.u, av: d.av };
-        k.join('global');
-        o.emit('ul', { online: _u, db: _db });
-        k.emit('hs', _msgs['global'] || []);
-    });
+        if(!d.ph) return;
+        
+        // Фікс дублів: Видаляємо старий нік, якщо номер той самий
+        for (let name in _db) {
+            if (_db[name].ph === d.ph && name !== d.u) {
+                delete _db[name]; delete _u[name];
+            }
+        }
 
-    k.on('join_room', (r) => {
-        k.leaveAll(); k.join(r);
-        k.emit('hs', _msgs[r] || []);
+        k.u = d.u;
+        _u[d.u] = { id: k.id, av: d.av };
+        _db[d.u] = { av: d.av, ph: d.ph }; 
+        
+        o.emit('ul', { online: _u, all: _db });
+        
+        // Синхронізація: Відправляємо історію
+        k.emit('hs', _h);
     });
 
     k.on('m', (d) => {
         d.id = 'm_' + Date.now();
-        const r = d.room || 'global';
-        if(!_msgs[r]) _msgs[r] = [];
-        _msgs[r].push(d);
-        if(_msgs[r].length > 50) _msgs[r].shift();
-        o.to(r).emit('m', d);
+        if(d.to) {
+            const t = _u[d.to];
+            if(t) o.to(t.id).to(k.id).emit('m', d);
+        } else {
+            _h.push(d); if(_h.length > 100) _h.shift();
+            o.emit('m', d);
+        }
     });
 
-    k.on('d', (id) => o.emit('d', id));
-    k.on('disconnect', () => { delete _u[k.u]; o.emit('ul', { online: _u, db: _db }); });
+    k.on('d', (i) => { _h = _h.filter(x => x.id !== i); o.emit('d', i); });
+    k.on('disconnect', () => { if(k.u) delete _u[k.u]; o.emit('ul', { online: _u, all: _db }); });
 });
-
-a.get('*', (q, r) => r.sendFile(p.join(__dirname, 'index.html')));
 s.listen(process.env.PORT || 3000);
