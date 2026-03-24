b57f3db2716fd395e65a999313d7e1190eb5d69f
NooMB1T <antonyarmoshuk@gmail.com>
2026-03-24 11:18:47 +0200

    server.js

--- server.js
+++ server.js
@@ -8,32 +8,27 @@ const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
-const io = new Server(server, { 
-    cors: { origin: "*" }, 
-    maxHttpBufferSize: 1e8 
-});
+const io = new Server(server, { cors: { origin: "*" }, maxHttpBufferSize: 1e8 });
 
 const mongoURI = "mongodb+srv://Dove:DoveChatIloveYou@dovechat.si1vone.mongodb.net/chat_db?retryWrites=true&w=majority&appName=DoveChat";
-
-mongoose.connect(mongoURI).then(() => console.log("БД Підключена!")).catch(err => console.log(err));
+mongoose.connect(mongoURI).then(() => console.log("DB OK")).catch(err => console.log(err));
 
 const Msg = mongoose.model('Msg', { u: String, t: String, c: String, h: String, d: { type: Date, default: Date.now } });
 
 let users = [];
 
 io.on('connection', (socket) => {
-    socket.on('join', async (name) => {
-        socket.uname = name;
-        if(!users.find(u => u.name === name)) users.push({ id: socket.id, name });
+    socket.on('join', async (n) => {
+        socket.uname = n;
+        if(!users.find(u => u.name === n)) users.push({ id: socket.id, name: n });
         io.emit('list', users);
-        const history = await Msg.find().sort({d: 1}).limit(50);
-        socket.emit('history', history);
+        const h = await Msg.find().sort({d: 1}).limit(50);
+        socket.emit('history', h);
     });
 
-    socket.on('msg', async (data) => {
-        const newMsg = new Msg(data);
-        await newMsg.save();
-        io.emit('msg', data);
+    socket.on('msg', async (d) => {
+        await new Msg(d).save();
+        io.emit('msg', d);
     });
 
     socket.on('disconnect', () => {
@@ -43,4 +38,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log("Сервер запущено на посилання dovechat.onrender.com"));
+server.listen(PORT, () => console.log("Server Live"));
