8d5576dc70ab84babd50ef66f0408ad8471b32d3
NooMB1T <antonyarmoshuk@gmail.com>
2026-03-24 13:53:08 +0200

    server.js

--- server.js
+++ server.js
@@ -1,46 +1,35 @@
-const e = require('express');
-const h = require('http');
-const { Server: S } = require('socket.io');
-const p = require('path');
-const a = e();
-const s = h.createServer(a);
-const o = new S(s, { cors: { origin: "*" }, maxHttpBufferSize: 1e8 });
+const e = require('express'), h = require('http'), { Server: S } = require('socket.io'), p = require('path');
+const a = e(), s = h.createServer(a), o = new S(s, { cors: { origin: "*" } });
 a.use(e.static(__dirname));
-let _h = []; 
-let _u = {}; 
+let _h = [], _u = {};
 a.get('/', (q, r) => r.sendFile(p.join(__dirname, 'index.html')));
 a.get('/chat.html', (q, r) => r.sendFile(p.join(__dirname, 'chat.html')));
-a.get('/chat', (q, r) => r.sendFile(p.join(__dirname, 'chat.html')));
 o.on('connection', (k) => {
-    k.on('join', (d) => {
-        k.u = d.u;
-        _u[k.id] = d.u;
-        k.join('global');
-        o.emit('ul', Object.values(_u));
+    k.on('j', (d) => {
+        if(!d.u) return;
+        k.u = d.u; _u[d.u] = k.id; k.join('g');
+        o.emit('ul', Object.keys(_u));
         k.emit('hs', _h);
     });
     k.on('m', (d) => {
-        d.id = Date.now().toString();
-        d.r = { '👍':0, '❤️':0, '😂':0 };
-        _h.push(d);
-        if(_h.length > 100) _h.shift();
-        o.emit('m', d); 
+        d.id = 'msg_' + Math.random().toString(36).substr(2, 9);
+        d.r = { '👍':0, '❤️':0, '🔥':0 };
+        if(d.to) { // Private msg
+            const sid = _u[d.to];
+            if(sid) { o.to(sid).to(k.id).emit('m', d); }
+        } else { // Global msg
+            _h.push(d); if(_h.length > 50) _h.shift();
+            o.emit('m', d);
+        }
     });
-    k.on('t', (v) => {
-        k.broadcast.emit('t', { u: k.u, v });
-    });
-    k.on('d', (i) => {
-        _h = _h.filter(m => m.id !== i);
-        o.emit('d', i);
-    });
+    k.on('d', (i) => { _h = _h.filter(x => x.id !== i); o.emit('d', i); });
     k.on('re', (d) => {
         const m = _h.find(x => x.id === d.id);
         if(m) { m.r[d.e]++; o.emit('up', m); }
     });
     k.on('disconnect', () => {
-        delete _u[k.id];
-        o.emit('ul', Object.values(_u));
+        if(k.u) delete _u[k.u];
+        o.emit('ul', Object.keys(_u));
     });
 });
-const P = process.env.PORT || 3000;
-s.listen(P);
+s.listen(process.env.PORT || 3000);
