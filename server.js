920eb683fa7eb599d4f87714e33614a44a96a3a9
NooMB1T <antonyarmoshuk@gmail.com>
2026-03-24 14:56:11 +0200

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const e = require('express'), h = require('http'), { Server: S } = require('socket.io'), p = require('path');
 const a = e(), s = h.createServer(a), o = new S(s, { cors: { origin: "*" } });
-
 a.use(e.static(__dirname));
 let _h = [], _u = {};
 
@@ -13,25 +12,16 @@ o.on('connection', (k) => {
         k.u = d.u; _u[d.u] = { id: k.id, av: d.av, ph: d.ph };
         o.emit('ul', _u); k.emit('hs', _h);
     });
-
+    k.on('t', (d) => { k.broadcast.emit('t', { u: k.u, s: d.s }); }); // Typing logic
     k.on('m', (d) => {
         d.id = 'm_' + Date.now(); d.r = { '👍': [] };
-        if(d.to) { 
-            const t = _u[d.to]; if(t) o.to(t.id).to(k.id).emit('m', d);
-        } else { 
-            _h.push(d); if(_h.length > 60) _h.shift(); o.emit('m', d);
-        }
+        if(d.to) { const t = _u[d.to]; if(t) o.to(t.id).to(k.id).emit('m', d); } 
+        else { _h.push(d); if(_h.length > 100) _h.shift(); o.emit('m', d); }
     });
-
     k.on('d', (i) => {
         const m = _h.find(x => x.id === i);
-        // Твои админ-права: NooMB1T может всё
-        if(k.u === 'NooMB1T' || (m && m.u === k.u)) {
-            _h = _h.filter(x => x.id !== i);
-            o.emit('d', i);
-        }
+        if(k.u === 'NooMB1T' || (m && m.u === k.u)) { _h = _h.filter(x => x.id !== i); o.emit('d', i); }
     });
-
     k.on('disconnect', () => { if(k.u) delete _u[k.u]; o.emit('ul', _u); });
 });
 s.listen(process.env.PORT || 3000);
