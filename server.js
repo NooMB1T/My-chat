f1d6a28fe110665a3c1f7be82950c6da8dad8f6c
NooMB1T <antonyarmoshuk@gmail.com>
2026-03-24 11:42:43 +0200

    server.js

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
+
+// Эта строка заставляет сервер "видеть" твои файлы index.html и chat.html
+app.use(express.static(__dirname));
 
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
@@ -20,7 +24,10 @@ io.on('connection', (socket) => {
     });
 });
 
-app.get('/', (req, res) => { res.send('Server Active'); });
+// Эта строка говорит серверу: "Когда заходят на сайт, покажи index.html"
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log("Live"));
