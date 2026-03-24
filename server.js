9ca458e286d6254c65e34f5ad7421fbe05cfceec
NooMB1T <antonyarmoshuk@gmail.com>
2026-03-24 11:31:19 +0200

    server.js

--- server.js
+++ server.js
@@ -9,19 +9,18 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-// Храним последние 20 сообщений в памяти (пока сервер не рестартнет)
 let history = []; 
 
 io.on('connection', (socket) => {
-    // Сразу отправляем историю новичку
     socket.emit('history', history);
-
     socket.on('msg', (data) => {
         history.push(data);
-        if(history.length > 20) history.shift(); // Лимит истории
-        io.emit('msg', data); // Рассылаем всем
+        if(history.length > 30) history.shift();
+        io.emit('msg', data);
     });
 });
 
+app.get('/', (req, res) => { res.send('Server Active'); });
+
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log("Работаем без БД!"));
+server.listen(PORT, () => console.log("Live"));
