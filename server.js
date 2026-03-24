6c3f781811d46abadc2a73faa6c6246b725db1c2
NooMB1T <antonyarmoshuk@gmail.com>
2026-03-24 12:27:45 +0200

    server.js

--- server.js
+++ server.js
@@ -5,17 +5,13 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { 
-    cors: { origin: "*" },
-    maxHttpBufferSize: 1e8 // Дозволяє надсилати великі фото
-});
+const io = new Server(server, { cors: { origin: "*" }, maxHttpBufferSize: 1e8 });
 
 app.use(express.static(__dirname));
 
 let history = []; 
 
 io.on('connection', (socket) => {
-    // Користувач входить у конкретний чат (наприклад, 'global')
     socket.on('join', (room) => {
         socket.join(room);
         const roomMsgs = history.filter(m => m.r === room);
@@ -25,12 +21,12 @@ io.on('connection', (socket) => {
     socket.on('msg', (data) => {
         history.push(data);
         if(history.length > 100) history.shift();
-        // Надсилаємо повідомлення тільки людям у цій кімнаті
         io.to(data.r).emit('msg', data); 
     });
 });
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/chat', (req, res) => res.sendFile(path.join(__dirname, 'chat.html')));
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log("Live"));
