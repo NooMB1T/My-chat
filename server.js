ef324576ba7926447b45cc267c815840c2917422
NooMB1T <antonyarmoshuk@gmail.com>
2026-03-24 16:31:58 +0200

    server.js

--- server.js
+++ server.js
@@ -2,24 +2,33 @@ const e = require('express'), h = require('http'), { Server: S } = require('sock
 const a = e(), s = h.createServer(a), o = new S(s, { cors: { origin: "*" } });
 
 a.use(e.static(__dirname));
-let _h = [], _u = {}, _bg = null; // Глобальний фон чату
+let _h = [], _u = {}, _bg = "https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?q=80&w=1974&auto=format&fit=crop";
 
 o.on('connection', (k) => {
     k.on('j', (d) => {
         k.u = d.u;
-        _u[d.u] = { id: k.id, av: d.av, ph: d.ph };
+        _u[d.u] = { id: k.id, av: d.av, ph: d.ph, bio: d.bio || "Пользователь DoveGram" };
         o.emit('ul', _u);
         k.emit('hs', { h: _h, bg: _bg });
     });
 
     k.on('m', (d) => {
         d.id = 'm_' + Date.now();
+        d.re = {}; // Реакции
         _h.push(d); 
         if(_h.length > 100) _h.shift();
         o.emit('m', d);
     });
 
-    k.on('set_bg', (url) => { _bg = url; o.emit('new_bg', url); });
+    k.on('typing', (v) => { k.broadcast.emit('is_typing', { u: k.u, v }); });
+
+    k.on('react', (d) => {
+        const m = _h.find(x => x.id === d.id);
+        if(m) {
+            m.re[d.u] = d.r; // Один юзер - одна реакция
+            o.emit('update_m', m);
+        }
+    });
 
     k.on('d', (id) => {
         _h = _h.filter(x => x.id !== id);
