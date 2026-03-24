91e7e360cc94e67e9af2528e4b1429998a0c996b
NooMB1T <antonyarmoshuk@gmail.com>
2026-03-24 19:13:46 +0200

    server.js

--- server.js
+++ server.js
@@ -2,33 +2,50 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+const io = require('socket.io')(server, {
+  cors: { origin: "*" }
 });
 
-let messages = [], users = {};
+app.use(express.static(__dirname));
+
+let messages = []; // Сховище Глобального чату
+let users = {};
 
 io.on('connection', (socket) => {
+    // Вхід у систему
     socket.on('join', (data) => {
         socket.username = data.u;
         users[data.u] = { id: socket.id, av: data.av, ph: data.ph };
-        io.emit('ul', users);
-        socket.emit('hs', messages);
+        io.emit('ul', users); // Оновити список онлайн
+        socket.emit('hs', messages); // Віддати історію чату
     });
 
+    // Обробка повідомлень (текст + фото)
     socket.on('msg', (m) => {
-        messages.push(m);
-        if (messages.length > 50) messages.shift();
-        io.emit('msg', m);
+        const fullMsg = { 
+            u: m.u, 
+            t: m.t, 
+            img: m.img || null, 
+            id: Date.now(), 
+            re: [] 
+        };
+        messages.push(fullMsg);
+        if (messages.length > 100) messages.shift();
+        io.emit('msg', fullMsg);
+    });
+
+    // Реакції
+    socket.on('add_re', (data) => {
+        io.emit('update_re', data);
     });
 
     socket.on('disconnect', () => {
-        if (socket.username) { delete users[socket.username]; io.emit('ul', users); }
+        if (socket.username) {
+            delete users[socket.username];
+            io.emit('ul', users);
+        }
     });
 });
 
-module.exports = server;
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`DoveGram Core on port ${PORT}`));
